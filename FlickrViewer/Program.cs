643f1cdfa8bba37fa9cadf32969e2721d5059792
Jay611 <nanbaejjae@gmail.com>
2020-04-10 08:38:55 -0400

    Add new project - FlickrViewer

---
+++ FlickrViewer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlickrViewer
+{
+    static class Program
+    {
+        /*Question 2
+
+        You are asked to use Parallel class to modify FlickrViewer app 
+        by adding image resizing functionality. More specifically, 
+        resize found image(s) and save these resized image locally. 
+        You can find the details about how to resize the image  
+        from https://www.andrewhoefling.com/Home/post/basic-image-manipulation-in-c-sharp
+
+         */
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FickrViewerForm());
+        }
+    }
+}
