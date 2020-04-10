cc88884b1e053bef650d140a638dabf4dc5452fd
Jay611 <nanbaejjae@gmail.com>
2020-04-10 12:31:50 -0400

    Initial FickrViewerForm - add resizing and saving image function

--- FlickrViewer/FickrViewerForm.cs
+++ FlickrViewer/FickrViewerForm.cs
@@ -1,119 +1,155 @@
 ﻿// Fig. 23.4: FickrViewerForm.cs
 // Invoking a web service asynchronously with class HttpClient
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
 namespace FlickrViewer
 {
-   public partial class FickrViewerForm : Form
-   {
+    public partial class FickrViewerForm : Form
+    {
         // Use your Flickr API key here--you can get one at:
         // https://www.flickr.com/services/apps/create/apply
-        private const string KEY = "your Flickr api key";
+        private const string KEY = "96d545f74ba9a9945f6baa08118bc7ca";
+        // password "b5eedee1d11bee17"
 
-      // object used to invoke Flickr web service      
-      private static HttpClient flickrClient = new HttpClient();
+        // object used to invoke Flickr web service      
+        private static HttpClient flickrClient = new HttpClient();
 
-      Task<string> flickrTask = null; // Task<string> that queries Flickr
+        Task<string> flickrTask = null; // Task<string> that queries Flickr
 
-      public FickrViewerForm()
-      {
-         InitializeComponent();
-      }
+        public FickrViewerForm()
+        {
+            InitializeComponent();
+        }
 
-      // initiate asynchronous Flickr search query; 
-      // display results when query completes
-      private async void searchButton_Click(object sender, EventArgs e)
-      {
-         // if flickrTask already running, prompt user 
-         if (flickrTask?.Status != TaskStatus.RanToCompletion)
-         {
-            var result = MessageBox.Show(
-               "Cancel the current Flickr search?",
-               "Are you sure?", MessageBoxButtons.YesNo,
-               MessageBoxIcon.Question);
-
-            // determine whether user wants to cancel prior search
-            if (result == DialogResult.No)
+        // initiate asynchronous Flickr search query; 
+        // display results when query completes
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            // if flickrTask already running, prompt user 
+            if (flickrTask?.Status != TaskStatus.RanToCompletion)
             {
-               return;
+                var result = MessageBox.Show(
+                   "Cancel the current Flickr search?",
+                   "Are you sure?", MessageBoxButtons.YesNo,
+                   MessageBoxIcon.Question);
+
+                // determine whether user wants to cancel prior search
+                if (result == DialogResult.No)
+                {
+                    return;
+                }
+                else
+                {
+                    flickrClient.CancelPendingRequests(); // cancel search
+                }
             }
-            else
+
+            // Flickr's web service URL for searches                         
+            var flickrURL = "https://api.flickr.com/services/rest/?method=" +
+               $"flickr.photos.search&api_key={KEY}&" +
+               $"tags={inputTextBox.Text.Replace(" ", ",")}" +
+               "&tag_mode=all&per_page=500&privacy_filter=1";
+
+            imagesListBox.DataSource = null; // remove prior data source
+            imagesListBox.Items.Clear(); // clear imagesListBox
+            pictureBox.Image = null; // clear pictureBox
+            imagesListBox.Items.Add("Loading..."); // display Loading...
+
+            // invoke Flickr web service to search Flick with user's tags
+            flickrTask = flickrClient.GetStringAsync(flickrURL);
+
+            // await flickrTask then parse results with XDocument and LINQ
+            XDocument flickrXML = XDocument.Parse(await flickrTask);
+
+            // gather information on all photos
+            var flickrPhotos =
+               from photo in flickrXML.Descendants("photo")
+               let id = photo.Attribute("id").Value
+               let title = photo.Attribute("title").Value
+               let secret = photo.Attribute("secret").Value
+               let server = photo.Attribute("server").Value
+               let farm = photo.Attribute("farm").Value         
+               where farm != "0" //WebException: The remote name could not be resolved: 'farm0.staticflickr.com'
+               select new FlickrResult
+               {
+                   Title = title,
+                   URL = $"https://farm{farm}.staticflickr.com/" +
+                     $"{server}/{id}_{secret}.jpg"
+               };
+            imagesListBox.Items.Clear(); // clear imagesListBox
+
+            // set ListBox properties only if results were found
+            if (flickrPhotos.Any())
             {
-               flickrClient.CancelPendingRequests(); // cancel search
+                imagesListBox.DataSource = flickrPhotos.ToList();
+                imagesListBox.DisplayMember = "Title";
+                SaveImage(flickrPhotos);
             }
-         }
-
-         // Flickr's web service URL for searches                         
-         var flickrURL = "https://api.flickr.com/services/rest/?method=" +
-            $"flickr.photos.search&api_key={KEY}&" +
-            $"tags={inputTextBox.Text.Replace(" ", ",")}" +
-            "&tag_mode=all&per_page=500&privacy_filter=1";
-
-         imagesListBox.DataSource = null; // remove prior data source
-         imagesListBox.Items.Clear(); // clear imagesListBox
-         pictureBox.Image = null; // clear pictureBox
-         imagesListBox.Items.Add("Loading..."); // display Loading...
-
-         // invoke Flickr web service to search Flick with user's tags
-         flickrTask = flickrClient.GetStringAsync(flickrURL);
-
-         // await flickrTask then parse results with XDocument and LINQ
-         XDocument flickrXML = XDocument.Parse(await flickrTask);
-
-         // gather information on all photos
-         var flickrPhotos =
-            from photo in flickrXML.Descendants("photo")
-            let id = photo.Attribute("id").Value
-            let title = photo.Attribute("title").Value
-            let secret = photo.Attribute("secret").Value
-            let server = photo.Attribute("server").Value
-            let farm = photo.Attribute("farm").Value
-            select new FlickrResult
+            else // no matches were found
             {
-               Title = title,
-               URL = $"https://farm{farm}.staticflickr.com/" +
-                  $"{server}/{id}_{secret}.jpg"
-            };
-         imagesListBox.Items.Clear(); // clear imagesListBox
-
-         // set ListBox properties only if results were found
-         if (flickrPhotos.Any())
-         {
-            imagesListBox.DataSource = flickrPhotos.ToList();
-            imagesListBox.DisplayMember = "Title";
-         }
-         else // no matches were found
-         {
-            imagesListBox.Items.Add("No matches");
-         }
-      }
-
-      // display selected image
-      private async void imagesListBox_SelectedIndexChanged(
-         object sender, EventArgs e)
-      {
-         if (imagesListBox.SelectedItem != null)
-         {
-            string selectedURL = ((FlickrResult) imagesListBox.SelectedItem).URL;
-
-            // use HttpClient to get selected image's bytes asynchronously
-            byte[] imageBytes = await flickrClient.GetByteArrayAsync(selectedURL);
-
-            // display downloaded image in pictureBox                  
-            using (var memoryStream = new MemoryStream(imageBytes))
-            {
-               pictureBox.Image = Image.FromStream(memoryStream);
+                imagesListBox.Items.Add("No matches");
             }
-         }
-      }
-   }
+        }
+
+        // display selected image
+        private async void imagesListBox_SelectedIndexChanged(
+           object sender, EventArgs e)
+        {
+            if (imagesListBox.SelectedItem != null)
+            {
+                string selectedURL = ((FlickrResult)imagesListBox.SelectedItem).URL;
+
+                // use HttpClient to get selected image's bytes asynchronously
+                byte[] imageBytes = await flickrClient.GetByteArrayAsync(selectedURL);
+
+                // display downloaded image in pictureBox                  
+                using (var memoryStream = new MemoryStream(imageBytes))
+                {
+                    pictureBox.Image = Image.FromStream(memoryStream);
+                }
+            }
+        }
+        //sava images 
+        private void SaveImage(IEnumerable<FlickrResult> flickrPhotos)
+        {
+            ParallelLoopResult result = Parallel.ForEach<FlickrResult>(flickrPhotos, async photo => {
+
+                byte[] imageBytes = await flickrClient.GetByteArrayAsync(photo.URL);
+                byte[] resizedImageBytes = ResizeImage(imageBytes, 300);
+                string path = "C:\\Software Engineering\\2020 Winter\\Programming3\\Assignment\\301005189(Bae)_Lab5\\301005189(Bae)_Lab05\\FlickrViewer\\Images\\";
+                string filaName = Regex.Match(photo.URL, @"[^//]*$").Value;
+
+                File.WriteAllBytes(path + filaName, resizedImageBytes);
+            });
+        }
+        //resizing images with width
+        private byte[] ResizeImage(byte[] data, int width)
+        {
+            using (var stream = new MemoryStream(data))
+            {
+                var image = Image.FromStream(stream);
+
+                var height = (width * image.Height) / image.Width;
+                var thumbnail = image.GetThumbnailImage(width, height, null, IntPtr.Zero);
+
+                using (var thumbnailStream = new MemoryStream())
+                {
+                    thumbnail.Save(thumbnailStream, ImageFormat.Jpeg);
+                    return thumbnailStream.ToArray();
+                }
+            }
+        }
+    }
 }
 
 /**************************************************************************
